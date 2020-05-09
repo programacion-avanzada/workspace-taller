dff3b01dc36d7832a692144b54ee447414e2cced
Lucas Videla <videla.lucas@gmail.com>
2020-05-09 12:25:40 -0300

    Se agrega ejemplo de pruebas

---
+++ pruebas-junit/src/edu/unlam/taller/pruebas/Punto.java
@@ -0,0 +1,23 @@
+package edu.unlam.taller.pruebas;
+
+public class Punto {
+	double x, y;
+
+	public Punto(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public double getModulo() {
+		return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+	}
+	
+}
